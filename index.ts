b4cb0830d3dc9559348bbfe99642a265dc0c3b6e
CRBroughton <crbroughton@posteo.uk>
2024-02-11 14:07:38 +0000

    add comments for targetMap, depsMap and dep

--- index.ts
+++ index.ts
@@ -1,12 +1,18 @@
 export function reactivity() {
     let activeEffect: Function | null = null
 
+    // targetMap stores all of the dependencies
+    // for each reactive object's properties
     const targetMap = new WeakMap<Object, Map<string | symbol, Set<Function | null>>>()
     function track<T extends Object>(target: T, key: string | symbol) {
+        // this dependency map stores dependencies
+        // for each property in the targetMap
         let depsMap = targetMap.get(target)
         if (!depsMap) {
             targetMap.set(target, (depsMap = new Map()))
         }
+        // a 'dep' is a set of effects that will
+        // re-run when their dependant values change
         let dep = depsMap.get(key)
         if (!dep) {
             depsMap.set(key, (dep = new Set()))
