a5cf688062c2b4c261c225754d2063a2bf65cb59
CRBroughton <crbroughton@posteo.uk>
2024-11-24 19:12:40 +0000

    feat(signals-v2): :sparkles: show working example

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import { createEffect, createSignal } from './signals';
+import typescriptLogo from './typescript.svg';
+import './style.css';
+import viteLogo from '/vite.svg';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
-    <a href="https://vite.dev" target="_blank">
+    <a href="https://vitejs.dev" target="_blank">
       <img src="${viteLogo}" class="logo" alt="Vite logo" />
     </a>
     <a href="https://www.typescriptlang.org/" target="_blank">
@@ -13,12 +13,44 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
     </a>
     <h1>Vite + TypeScript</h1>
     <div class="card">
-      <button id="counter" type="button"></button>
+      <button id="decrement" type="button">-</button>
+      <span id="count"></span>
+      <button id="increment" type="button">+</button>
+    </div>
+    <div class="card">
+      <input id="nameInput" type="text" placeholder="Enter your name">
+      <div id="greeting"></div>
     </div>
     <p class="read-the-docs">
       Click on the Vite and TypeScript logos to learn more
     </p>
   </div>
-`
+`;
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const [count, setCount] = createSignal(0);
+createEffect(() => {
+  const countElement = document.querySelector('#count');
+  if (countElement) {
+    countElement.innerHTML = `Count is: ${count()}`;
+  }
+});
+
+document.querySelector('#increment')?.addEventListener('click', () => {
+  setCount(c => c + 1);
+});
+document.querySelector('#decrement')?.addEventListener('click', () => {
+  setCount(c => c - 1);
+});
+
+const [name, setName] = createSignal('');
+createEffect(() => {
+  const greetingElement = document.querySelector('#greeting');
+  if (greetingElement) {
+    greetingElement.innerHTML = name()
+      ? `Hello, ${name()}!`
+      : 'Enter your name above';
+  }
+});
+document.querySelector('#nameInput')?.addEventListener('input', (e) => {
+  setName((e.target as HTMLInputElement).value);
+});
