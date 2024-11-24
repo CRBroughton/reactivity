d81285dc7a7f177c7f49adf1e07b1edf5f1a7e42
CRBroughton <crbroughton@posteo.uk>
2024-11-24 21:26:17 +0000

    feat(signals-v2): :sparkles: create custom createElement function

--- src/dom.ts
+++ src/dom.ts
@@ -6,14 +6,39 @@ interface VNode {
 }
 type Children = (VNode | string)[];
 
-function html(type: string, props: Props = {}, ...children: Children): VNode {
-  return {
-    type,
-    props,
-    children: children.flat(),
-  };
-}
+// TODO - Try to implement some sort of VDOM
+// function html(type: string, props: Props = {}, ...children: Children): VNode {
+//   return {
+//     type,
+//     props,
+//     children: children.flat(),
+//   };
+// }
 
-function text(content: string): string {
-  return content;
+// function text(content: string): string {
+//   return content;
+// }
+
+type Style = Partial<CSSStyleDeclaration | Record<string, PropertyKey>>;
+interface ElementProps {
+  style?: Style;
+  class?: string;
+  className?: string;
+  [key: PropertyKey]: any;
+};
+export function createElement(tag: string, props: ElementProps = {}): HTMLElement {
+  const element = document.createElement(tag);
+
+  if (props.className || props.class)
+    element.className = props.className || props.class || '';
+
+  if (props.style)
+    Object.assign(element.style, props.style);
+
+  Object.entries(props).forEach(([key, value]) => {
+    if (key !== 'style' && key !== 'class' && key !== 'className')
+      element.setAttribute(key, value);
+  });
+
+  return element;
 }
