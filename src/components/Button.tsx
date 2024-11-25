fe729b7de8328c54ce3b3817f571a6095dc1ca7d
CRBroughton <crbroughton@posteo.uk>
2024-11-25 20:04:06 +0000

    refactor: :recycle: move prop types to interface

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,16 +2,18 @@
 
 import { createElement } from '../dom';
 
-export function Button(
-  { text, click }:
-  { text: string; click: (event: MouseEvent) => void },
-) {
+interface ButtonProps {
+  text: string;
+  click: (event: MouseEvent) => void;
+
+}
+export function Button(props: ButtonProps) {
   return (
     <button
       class="px-4 py-2 text-white bg-green-500 rounded hover:bg-green-600 transition-colors"
-      onClick={click}
+      onClick={props.click}
     >
-      {text}
+      {props.text}
     </button>
   );
 }
